286f2550ba2d55ba8128b17d7302adfb27086320
Abdoulie <54741807+Netizen-gm@users.noreply.github.com>
2025-12-25 03:26:39 -0800

    Create role.service.ts

---
+++ frontend/src/app/services/role.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getRoles(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/roles`);
+  }
+
+  getRole(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/roles/${id}`);
+  }
+
+  createRole(role: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/roles`, role);
+  }
+
+  updateRole(id: string, role: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/roles/${id}`, role);
+  }
+
+  deleteRole(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/roles/${id}`);
+  }
+}
